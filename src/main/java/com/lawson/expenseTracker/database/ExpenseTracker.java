d5ee6505663acc2937f2be39000d7bdb681f72e4
Clawson13 <chavoris.lawson.jobs@gmail.com>
2021-08-09 15:41:14 -0400

    merged with text-file

--- src/main/java/com/lawson/expenseTracker/database/ExpenseTracker.java
+++ src/main/java/com/lawson/expenseTracker/database/ExpenseTracker.java
@@ -1,5 +1,8 @@
 package com.lawson.expenseTracker.database;
 
+import com.lawson.expenseTracker.enums.ExpenseCategory;
+import com.lawson.expenseTracker.enums.ItemCategory;
+import com.lawson.expenseTracker.enums.PurchaseMethod;
 import com.lawson.expenseTracker.model.Expense;
 import com.lawson.expenseTracker.model.Item;
 
@@ -8,7 +11,12 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class ExpenseTracker {
 
@@ -20,34 +28,118 @@ public class ExpenseTracker {
     }
 
     public static boolean createExpense(){
-        Statement stmt = null;
-        ResultSet rs = null;
-        try {
-            connection.createStatement();
-            rs = stmt.executeQuery("SELECT * FROM expense");
-            return true;
-        }catch (SQLException s){
-            s.printStackTrace();
-        }finally {
-            if(rs!=null) {
-                try {
-                    rs.close();
-                } catch (SQLException s) {
-                }
-            }
-            if(stmt!=null) {
-                try {
-                    stmt.close();
-                } catch (SQLException s) {
-                }
-            }
+        Scanner s = new Scanner(System.in);
+        SimpleDateFormat sdfd = new SimpleDateFormat("MM-dd-yyyy");
+        SimpleDateFormat sdft = new SimpleDateFormat("HH:mm");
+        int counter=1;
+        Expense expense;
+        List<Item> eItems;
+        double totalPrice = 0;
+        Date date;
+        Date time;
+        String place;
+        String location;
+        String text;
+        String[] items;
+
+        System.out.println("Enter Expense Information:");
+        System.out.println("Items (Enter as comma separated list): ");
+        text = s.nextLine();
+        items = text.split(",");
+        // if contains number,  character, or excessive spaces in name
+        eItems = createItems(items, s);
+
+        System.out.println("Expense Category: ");
+        for(ExpenseCategory e: ExpenseCategory.values()){
+            System.out.println("Enter " + counter + " - " + e);
+            counter++;
         }
-        return false;
+        ExpenseCategory eCat = convertExpenseCategory(s.nextInt());
+        counter =1;
+
+        System.out.println("Expense Purchase Method: ");
+        for(PurchaseMethod p: PurchaseMethod.values()){
+            System.out.println("Enter " + counter + " - " + p);
+            counter++;
+        }
+        PurchaseMethod purchaseMethod = convertPurchaseMethodCategory(s.nextInt());
+        s.nextLine();
+
+        for (Item eItem : eItems) {
+            totalPrice += eItem.getPrice();
+        }
+        System.out.println("Expense purchase date (MM-dd-yyyy):");
+        date = sdfd.parse(s.nextLine());//
+        System.out.println("Expense purchase time (HH:mm):");
+        time = sdft.parse(s.nextLine());
+        System.out.println("Expense purchase place:");
+        place = s.nextLine();
+        System.out.println("Expense purchase location:");
+        location = s.nextLine();
+
+        expense = new Expense(eItems, eCat, purchaseMethod, totalPrice, date, time,
+                place, location);
+
+        return storeExpense(expense);
     }
 
-//    public static boolean createItem(){//Idk about this one since you items should be attached to expenses
-//
-//    }
+    private static List<Item> createItems(String[] items, Scanner s){//Idk about this one since you items should be attached to expenses
+        List<Item> eItems = new ArrayList<>();
+        String description;
+        ItemCategory category;
+        double price;
+        int counter = 1;
+
+        for(String i: items){
+            i = i.trim();
+            System.out.println(i +" Description:");
+            description = s.nextLine();
+            System.out.println(i +" Category:");
+
+            for(ItemCategory ic: ItemCategory.values()){
+                System.out.println("Enter " + counter + " - " + ic);
+                counter++;
+            }
+            category = convertItemCategory(s.nextInt());
+            System.out.println(i +" price:");
+            price = s.nextDouble();
+            s.nextLine();
+            counter = 1;
+
+            eItems.add(new Item(i, description, category, price));
+        }
+        return eItems;
+    }
+
+    private static boolean storeExpense(Expense expense){
+        Statement stmt = null;
+        ResultSet rs = null;
+        if(expense.size()<1){
+            return false;
+        }else{
+            try {
+                connection.createStatement();
+                rs = stmt.executeQuery("SELECT * FROM expense");
+                return true;
+            }catch (SQLException s){
+                s.printStackTrace();
+            }finally {
+                if(rs!=null) {
+                    try {
+                        rs.close();
+                    } catch (SQLException s) {
+                    }
+                }
+                if(stmt!=null) {
+                    try {
+                        stmt.close();
+                    } catch (SQLException s) {
+                    }
+                }
+            }
+            return true;
+        }
+    }
 
     public static boolean getAllExpenses(){
         Statement stmt = null;
@@ -75,13 +167,13 @@ public class ExpenseTracker {
         return false;
     }
 
-    public static Expense getExpense(){
-
-    }
-
-    public static Item getItem(){
-
-    }
+//    public static Expense getExpense(){
+//
+//    }
+//
+//    public static Item getItem(){
+//
+//    }
 
 //    public static List<Expense> getExpenses(){
 //
@@ -106,4 +198,25 @@ public class ExpenseTracker {
 //    public static boolean deleteItem(){
 //
 //    }
+
+    private static ItemCategory convertItemCategory(int catNumber){
+        for(ItemCategory it: ItemCategory.values()){
+            if(catNumber-1 == it.ordinal()){ return it; }
+        }
+        return ItemCategory.RANDOM;
+    }
+
+    private static ExpenseCategory convertExpenseCategory(int catNumber){
+        for(ExpenseCategory e: ExpenseCategory.values()){
+            if(catNumber-1 == e.ordinal()){ return e; }
+        }
+        return ExpenseCategory.RANDOM;
+    }
+
+    private static PurchaseMethod convertPurchaseMethodCategory(int catNumber){
+        for(PurchaseMethod p: PurchaseMethod.values()){
+            if(catNumber-1 == p.ordinal()){ return p; }
+        }
+        return PurchaseMethod.UNKNOWNN;
+    }
 }
--- src/main/java/com/lawson/expenseTracker/database/SqlConnector.java
+++ src/main/java/com/lawson/expenseTracker/database/SqlConnector.java
@@ -21,6 +21,8 @@ public class SqlConnector {
 //        this.password = password;
 //    }
 
+
+
     private SqlConnector() {
     }
 
